aa4064de2a390e03cc208b21c5fadb4043c403c0
Adam Leyshon <aleyshon@thecodecache.net>
2019-03-05 18:35:15 +0000

    Updated config to production.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DATABASE_IP = '192.168.1.1'
+DATABASE_IP = '127.0.0.1'
 DATABASE_PORT = 6379
 LISTEN_ON_IP = '0.0.0.0'
 LISTEN_ON_PORT = 5000
@@ -6,11 +6,4 @@ DEBUG_MODE = False
 API_DB_CONFIG = {
     'm1': 1,
     'm2': 2
-}
-
-OLD_API_DB_CONFIG = {
-    'b18': 0,
-    'b19': 1,
-    'u1': 2,
-    'test': 15
 }
