54e64cd6c5baf7f075b2e43d1e994fa6812bcd83
Martyn Green <martyn.robert.green@gmail.com>
2020-08-16 15:25:12 +0100

    reverting ports changed by dev merge

--- docker/gunicorn_conf.py
+++ docker/gunicorn_conf.py
@@ -5,7 +5,7 @@ import os
 workers_per_core_str = os.getenv("WORKERS_PER_CORE", "2")
 web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
 host = os.getenv("HOST", "0.0.0.0")
-port = os.getenv("PORT", "1997")
+port = os.getenv("PORT", "1987")
 bind_env = os.getenv("BIND", None)
 use_loglevel = os.getenv("LOG_LEVEL", "info")
 if bind_env:
